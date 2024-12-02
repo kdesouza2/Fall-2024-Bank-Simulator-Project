90e9bbb76d0082090a26e8590fc76d6953d9ebfa
kdesouza2 <keyra.desouza@gmail.com>
2024-12-01 23:27:29 -0500

    EDIT CommandValidatorTest.java

--- src/test/java/banking/CommandValidatorTest.java
+++ src/test/java/banking/CommandValidatorTest.java
@@ -268,6 +268,97 @@ public class CommandValidatorTest {
 	///////////////////////////////////////////////////////////////////
 	///////////////////// WITHDRAWAL TESTS ////////////////////////////
 	///////////////////////////////////////////////////////////////////
+	@Test
+	void withdraw_negative_amount_from_checking_is_invalid() {
+		newBank.addAccount(testChecking);
+		boolean actual = commandValidator.validateCommand("withdraw 12345678 -10");
+		assertFalse(actual);
+	}
+
+	@Test
+	void withdraw_greater_than_400_from_checking_is_invalid() {
+		newBank.addAccount(testChecking);
+		boolean actual = commandValidator.validateCommand("withdraw 12345678 500");
+		assertFalse(actual);
+	}
+
+	@Test
+	void withdraw_amount_from_checking_less_than_balance_is_valid() {
+		newBank.addAccount(testChecking);
+		testChecking.deposit(300);
+		boolean actual = commandValidator.validateCommand("withdraw 12345678 30");
+		assertTrue(actual);
+	}
+
+	@Test
+	void withdraw_amount_from_checking_greater_than_balance_is_valid() {
+		newBank.addAccount(testChecking);
+		testChecking.deposit(100);
+		boolean actual = commandValidator.validateCommand("withdraw 12345678 150");
+		assertTrue(actual);
+	}
+
+	@Test
+	void withdraw_negative_amount_from_savings_is_invalid() {
+		newBank.addAccount(testSavings);
+		boolean actual = commandValidator.validateCommand("withdraw 12345678 -10");
+		assertFalse(actual);
+	}
+
+	@Test
+	void withdraw_greater_than_1000_from_savings_is_invalid() {
+		newBank.addAccount(testSavings);
+		boolean actual = commandValidator.validateCommand("withdraw 12345678 10000");
+		assertFalse(actual);
+	}
+
+	@Test
+	void withdraw_amount_from_savings_less_than_balance_is_valid() {
+		newBank.addAccount(testSavings);
+		testSavings.deposit(300);
+		boolean actual = commandValidator.validateCommand("withdraw 12345678 30");
+		assertTrue(actual);
+	}
+
+	@Test
+	void withdraw_amount_from_savings_greater_than_balance_is_valid() {
+		newBank.addAccount(testSavings);
+		testSavings.deposit(100);
+		boolean actual = commandValidator.validateCommand("withdraw 12345678 150");
+		assertTrue(actual);
+	}
+
+	@Test
+	void withdraw_from_unwithdrawable_savings_account_is_invalid() {
+		newBank.addAccount(testSavings);
+		testSavings.setWithdrawableFalse();
+		boolean actual = commandValidator.validateCommand("withdraw 12345678 100");
+		assertFalse(actual);
+	}
+
+	@Test
+	void withdraw_command_without_command_is_invalid() {
+		boolean actual = commandValidator.validateCommand("12345678 100");
+		assertFalse(actual);
+	}
+
+	@Test
+	void withdraw_without_account_id_is_invalid() {
+		boolean actual = commandValidator.validateCommand("withdraw 100");
+		assertFalse(actual);
+	}
+
+	@Test
+	void withdraw_without_amount_is_invalid() {
+		boolean actual = commandValidator.validateCommand("withdraw 12345678");
+		assertFalse(actual);
+	}
+
+	@Test
+	void withdraw_from_nonexistent_account_is_invalid() {
+		boolean actual = commandValidator.validateCommand("withdraw 2345678 100");
+		assertFalse(actual);
+	}
 
 	///////////////////////////////////////////////////////////////////
 	///////////////////// PASS TIME TESTS /////////////////////////////
